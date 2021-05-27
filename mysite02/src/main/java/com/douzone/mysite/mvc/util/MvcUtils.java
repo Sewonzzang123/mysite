40ae8f5ad3328eb4f1c0aed626b4cc1d2bb552c2
Sewonzzang123 <dkrlrhdrur@gmail.com>
2021-05-27 11:50:03 +0900

    user #1 + serverside include

--- mysite02/src/main/java/com/douzone/mysite/mvc/util/MvcUtils.java
+++ mysite02/src/main/java/com/douzone/mysite/mvc/util/MvcUtils.java
@@ -9,11 +9,16 @@ import javax.servlet.http.HttpServletResponse;
 
 public class MvcUtils {
 
-	public static void forward(String path, HttpServletRequest request, HttpServletResponse response) 
-		throws ServletException, IOException{
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/"+path+".jsp");
+	public static void forward(String path, HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/" + path + ".jsp");
 		rd.forward(request, response);
 
+	}
+
+	public static void redirect(String url, HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.sendRedirect(url);
 	}
 
 }
