e5133e8518af008e170f73fc8f85ac33303dd058
Sewonzzang123 <dkrlrhdrur@gmail.com>
2021-05-28 12:51:08 +0900

    update form

--- mysite02/src/main/java/com/douzone/mysite/repository/UserRepository.java
+++ mysite02/src/main/java/com/douzone/mysite/repository/UserRepository.java
@@ -78,17 +78,65 @@ public class UserRepository {
 			pstmt.setString(2, password);
 
 			rs = pstmt.executeQuery();
-			if(rs.next()) {
+			if (rs.next()) {
 				Long no = rs.getLong(1);
 				String name = rs.getString(2);
-				
+
 				result = new UserVo();
-				
+
 				result.setNo(no);
 				result.setName(name);
 			}
-			
-			
+
+		} catch (SQLException e) {
+			System.out.println("error " + e);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("error " + e);
+			}
+		}
+		return result;
+
+	}
+
+	public UserVo findByNo(Long userNo) {
+		UserVo result = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = getConnection();
+
+			String sql = "select name, email,gender " 
+						+ "from user " 
+						+ "where no =? ";
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setLong(1, userNo);
+
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				String name = rs.getString(1);
+				String email = rs.getString(2);
+				String gender = rs.getString(3);
+
+				result = new UserVo();
+				
+				result.setName(name);
+				result.setEmail(email);
+				result.setGender(gender);
+			}
 
 		} catch (SQLException e) {
 			System.out.println("error " + e);
