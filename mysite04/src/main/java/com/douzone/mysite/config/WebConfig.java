e5abda1e82cd3336ac59e67e408512fda2c5b777
Sewonzzang123 <dkrlrhdrur@gmail.com>
2021-06-15 10:47:27 +0900

    mysite04 #2

---
+++ mysite04/src/main/java/com/douzone/mysite/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.douzone.mysite.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
+
+import com.douzone.config.web.MvcConfig;
+//spring-servlet.xml을 대체해야 한다.
+/*
+ * <context:annotation-config />
+	<context:component-scan
+	base-package="com.douzone.mysite.controller,com.douzone.mysite.exception" />
+ * 
+ */
+@Configuration
+@EnableAspectJAutoProxy //<aop:aspectj-autoproxy />
+@ComponentScan({"com.douzone.mysite.controller, com.douzone.mysite.exception"})
+@Import({MvcConfig.class})
+public class WebConfig {
+
+
+		
+}
